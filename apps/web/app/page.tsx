b4978853d59d9369a8fcd3ad22f694809032fd78
kkratterf <kkratterf@gmail.com>
2024-05-29 16:34:43 +0200

    fix: toast component

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -17,10 +17,6 @@ export default function Home() {
           onClick={() =>
             toast("Event has been created", {
               description: "Sunday, December 03, 2023 at 9:00 AM",
-              action: {
-                label: "Undo",
-                onClick: () => console.log("Undo"),
-              },
             })
           }>
           Show toast
