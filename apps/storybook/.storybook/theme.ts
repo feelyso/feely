7b7c0ca04b7a561fd278938a73cbeb0151f4213a
kkratterf <kkratterf@gmail.com>
2024-05-30 21:44:33 +0200

    add favicon and logo @feely/storybook

---
+++ apps/storybook/.storybook/theme.ts
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "dark",
+  brandTitle: "feely",
+  brandUrl: "https://feely.so",
+  brandImage: "/public/logo/logo.png",
+  brandTarget: "_self",
+});
