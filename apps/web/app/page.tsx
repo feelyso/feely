38be790691af1cdd79a070d61b0af28e47eb4f90
kkratterf <kkratterf@gmail.com>
2024-05-30 17:17:50 +0200

    fix: next link

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 // Import core
+import Link from "next/link";
 // Import custom
 import { GridBackground } from "@components/waitlist/grid-background";
 import WaitlistForm from "@components/waitlist/waitlist-form";
@@ -9,7 +10,9 @@ import Pictogram from "@components/waitlist/pictogram";
 export default function Home() {
   return (
     <main className="h-screen w-screen">
-      <Pictogram />
+      <Link href="/">
+        <Pictogram />
+      </Link>
       <WaitlistForm />
       <GridBackground />
     </main>
