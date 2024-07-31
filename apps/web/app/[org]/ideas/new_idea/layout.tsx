e41d95635b3c5605c480c69efd1eb5a030f2875e
kkratterf <kkratterf@gmail.com>
2024-07-31 13:49:52 +0200

    Feat / New homepage

--- apps/web/app/[org]/ideas/new_idea/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Suspense } from "react";
-
-export default async function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>;
-}
--- apps/web/app/redirect_to_workspace/layout.tsx
+++
@@ -1,8 +0,0 @@
-import { Suspense } from "react";
-export default async function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <Suspense fallback={<div>Loading your workspace...</div>}>{children}</Suspense>;
-}
